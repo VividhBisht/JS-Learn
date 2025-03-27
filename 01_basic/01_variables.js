871447703a79290603c49b219f9b5f8158ea6e65
Vividh bisht <161566944+VividhBisht@users.noreply.github.com>
2025-03-27 06:47:38 +0000

    Variables and DataTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,27 @@
+const AccountId=14525;
+let accountEmail="Vividh5@gmail.com";
+var accountPassword="13235";
+accountCity="chandigarh";
+let accountState;
+
+/*  let accountState;
+ when you variable declare or not the assign declare then it would be UNDEFINED
+ */
+
+// AccountId=2;  // beacause it is const not change the AccountId
+
+
+accountEmail="vividh8@gmail.com";
+accountPassword="545454";
+accountCity="SAS";
+
+console.log(AccountId);
+
+/*
+prefer not use to VAR in JavaScript
+because of issue in block scope or functional scope
+*/
+
+
+
+console.table([AccountId,accountEmail, accountPassword, accountCity,accountState]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";  //treat all js code as a newer version
+
+alert(3+3); // this not work because we use NodeJs, not Browser
+
+let name="Bisht" // this is string
+let age=18  // this is Number
+let loggedIn=true  // this is boolean
+
+/*
+  Number
+  BigInt
+  String => ""
+  Boolean => true/false
+  null => its typeOf is Object
+  undefined
+*/
