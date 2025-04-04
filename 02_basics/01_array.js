1d10c3c7d8495082e46fddc3a6bd1d869f3607b8
Vividh bisht <161566944+VividhBisht@users.noreply.github.com>
2025-04-04 07:47:41 +0000

    Arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,12 @@
+// Array
+
+const myArray=[0,1,2,3,4,5]
+console.log([1])
+
+// Array Methods
+
+// myArray.push(6);   //value Add in array
+// myArray.push(7);
+// myArray.pop();   // last value remove from array
+
+console.log(myArray);
