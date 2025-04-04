11cc57626ed4bd03d03118bd672620044bc1c445
Vividh bisht <161566944+VividhBisht@users.noreply.github.com>
2025-04-04 06:59:18 +0000

    nums and math

---
+++ 01_basic/06_nums_maths.js
@@ -0,0 +1,11 @@
+const Runs=400
+// console.log(Runs);
+
+
+const balance =new Number(100)
+// console.log(balance);
+
+/////// Maths //////////////
+console.log(Math);
+console.log(Math.abs(-4));   // only Negative value convert into positive
+console.log(Math.round(44.6))  
